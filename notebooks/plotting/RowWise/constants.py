d4038d315933655109f8e3f964cd666db0b46aa1
Max <mcschrader@crimson.ua.edu>
2023-02-14 16:12:21 -0600

    updated notebooks
    Signed-off-by: Max <mcschrader@crimson.ua.edu>

--- notebooks/plotting/RowWise/constants.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-experiment_path = Path("/Volumes/BittleLab/7 DOE Project/sumo-uc-2023/RowWiseSampling/02.08.2023_10.32.54")
-default_experiment_path = Path("/Volumes/BittleLab/7 DOE Project/sumo-uc-2023/CarFollowingDefaults/02.08.2023_09.07.12")
-
---
+++ notebooks/plotting/real-world/constants.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+experiment_path = Path("/Volumes/BittleLab/7 DOE Project/sumo-uc-23/SampledDistribution/02.12.2023_13.28.23")
+default_experiment_path = Path("/Volumes/BittleLab/7 DOE Project/sumo-uc-23/CarFollowingDefaults/02.13.2023_07.51.08")
+

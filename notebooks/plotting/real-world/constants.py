65a2de86a8d8abc2d3e20d1dbdd191dc2c73f5a9
Max <mcschrader@crimson.ua.edu>
2023-02-16 05:49:22 -0600

    paper ready notebooks?
    Signed-off-by: Max <mcschrader@crimson.ua.edu>

--- notebooks/plotting/real-world/constants.py
+++ notebooks/plotting/real-world/constants.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-experiment_path = Path("/Volumes/BittleLab/7 DOE Project/sumo-uc-23/SampledDistribution/02.12.2023_13.28.23")
+experiment_path = Path("/Volumes/BittleLab/7 DOE Project/sumo-uc-23/SampledDistribution/02.14.2023_06.21.29")
 default_experiment_path = Path("/Volumes/BittleLab/7 DOE Project/sumo-uc-23/CarFollowingDefaults/02.13.2023_07.51.08")
 
